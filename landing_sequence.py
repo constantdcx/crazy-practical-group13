7e0d95afa740782de1c1e0e9f1612c02d96455f1
ArthurSalamin <91951731+ArthurSalamin@users.noreply.github.com>
2022-05-24 16:17:49 +0200

    landing_sequence

--- landing_sequence.py
+++ landing_sequence.py
@@ -12,6 +12,7 @@ Crazyflie.
 import logging
 import sys
 import time
+import keyboard
 
 import cflib.crtp
 from cflib.crazyflie.log import LogConfig
@@ -79,7 +80,7 @@ def descending_step():
 def landing_sequence(vel_x,vel_y):
     print('landing sequence')
     VELOCITY = 0.5
-    epsilon = 10
+    epsilon = 0.01
 
     # look for mid pos on first direction (pos_m1)
     motion_commander.start_linear_motion(vel_x, vel_y, 0)
@@ -97,19 +98,19 @@ def landing_sequence(vel_x,vel_y):
         time.sleep(0.1)
 
     # look for mid pos on second direction (pos_m2)
-    motion_commander.start_linear_motion(-vel_x, vel_y, 0)
+    motion_commander.start_linear_motion(-vel_y, vel_x, 0)
     while not descending_step():
         #keep flying
         time.sleep(0.1)
     pos_1 = [log_pos[-1][0],log_pos[-1][1]]
-    motion_commander.start_linear_motion(vel_x, -vel_y, 0)
+    motion_commander.start_linear_motion(-vel_x, -vel_y, 0)
     while not descending_step():
         #keep flying
         time.sleep(0.1)
     pos_2 = [log_pos[-1][0],log_pos[-1][1]]
     pos_m2 = [(pos_1[0]-pos_2[0])/2,(pos_1[1]-pos_2[1])/2]
 
-    motion_commander.start_linear_motion(-vel_x, vel_y, 0)
+    motion_commander.start_linear_motion(-vel_x, -vel_y, 0)
     while ((log_pos[-1][0]-pos_m2[0])**2+(log_pos[-1][1]-pos_m2[1])**2)**0.5 > epsilon:
         #keep flying
         time.sleep(0.1)
@@ -136,9 +137,19 @@ if __name__ == '__main__':
                 state = "goal following"
                 print("switched to" + state)
                 start_position_printing(scf)
+                epsilon = 0.01
+                # current velocity
+                velocity_x = 0.0
+                velocity_y = 0.0
 
                 while keep_flying:
+
+                    landing_state = 0
+
                     #print(state)
+                    if keyboard.read_key() == "c":
+                        sys.exit()
+
                     if state == "obstacle avoidance":
                         #print(log_pos[-1]) #[0]) len(log_pos)
                         VELOCITY = 0.5
@@ -182,11 +193,64 @@ if __name__ == '__main__':
                     if is_close(multi_ranger.up):
                         keep_flying = False
 
-                    if (log_pos[-3][2]-log_pos[-1][2])> 0.05:
-                        print('base detectée')
+                    if (log_pos[-3][2]-log_pos[-1][2])> 0.05 or state == "landing":
                         #motion_commander.land()
-                        landing_sequence(velocity_x,velocity_y)
-                        keep_flying = False
+                        #landing_sequence(velocity_x,velocity_y)
+
+                        if landing_state == 0:
+                            print('base detectée')
+                            state = "landing"
+                            landing_state = 1
+                            pos_1 = [log_pos[-1][0],log_pos[-1][1]]
+
+                        if landing_state == 1:
+                            # look for mid pos on first direction (pos_m1)
+                            #motion_commander.start_linear_motion(vel_x, vel_y, 0)
+                            if descending_step():
+                                pos_2 = [log_pos[-1][0],log_pos[-1][1]]
+                                pos_m1 = [(pos_1[0]-pos_2[0])/2,(pos_1[1]-pos_2[1])/2]
+                                landing_state = 2
+                                #motion_commander.start_linear_motion(-vel_x, -vel_y, 0)
+                                velocity_y = -velocity_x
+                                velocity_y = -velocity_y
+
+                        #go to pos_m1
+                        if landing_state == 1:
+                            if ((log_pos[-1][0]-pos_m1[0])**2+(log_pos[-1][1]-pos_m1[1])**2)**0.5 > epsilon:
+                                landing_state = 3
+                                #motion_commander.start_linear_motion(-vel_x, vel_y, 0)
+                                v_temp = velocity_x
+                                velocity_x = -velocity_y
+                                velocity_y = v_temp
+
+
+                        # look for mid pos on second direction (pos_m2)
+                        if landing_state == 3:
+                            if descending_step():
+                                pos_1 = [log_pos[-1][0],log_pos[-1][1]]
+                                #motion_commander.start_linear_motion(vel_x, -vel_y, 0)
+                                velocity_x = -velocity_x
+                                velocity_y = -velocity_y
+                                landing_state = 4
+
+                        if landing_state == 4:
+                            if descending_step():
+                                pos_2 = [log_pos[-1][0],log_pos[-1][1]]
+                                pos_m2 = [(pos_1[0]-pos_2[0])/2,(pos_1[1]-pos_2[1])/2]
+                                #motion_commander.start_linear_motion(-vel_x, vel_y, 0)
+                                velocity_x = -velocity_x
+                                velocity_y = -velocity_y
+                                landing_state = 5
+
+                        if landing_state == 5:
+                            if ((log_pos[-1][0]-pos_m2[0])**2+(log_pos[-1][1]-pos_m2[1])**2)**0.5 > epsilon:
+                                #motion_commander.start_linear_motion(0, 0, 0)
+                                velocity_x = 0.0
+                                velocity_y = 0.0
+                                motion_commander.land()
+                                landing_state = 0
+                                keep_flying = False
+                                state = 'landed'
 
                     print('z: ', log_pos[-1][2])
                     print('x: ', log_pos[-1][0])
